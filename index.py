717c2e061ce961091b4f5bd7e0cce5312c216867
marlon307 <marlon-ramosb@hotmail.com>
2022-06-18 14:41:10 -0300

    Add requirements

--- index.py
+++ index.py
@@ -3,14 +3,15 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 
 @app.route("/")
-def hello_world():
+def index():
     return {
         "message": "Todos os serviços estão ativos no meomento.",
         "date": datetime.now(),
         "status": 200,
     }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
