02d4a975816fbfb6c252aae6d0cb3ee1e49f21d6
marlon307 <marlon-ramosb@hotmail.com>
2022-06-24 18:36:13 -0300

    Valid email and login

--- middleware/m_user.py
+++ middleware/m_user.py
@@ -1,7 +1,5 @@
 from flask import request
 from utility.m_credentials import valid_email, valid_psw
-from utility.generat_id import generate_id
-import uuid
 
 msgErr = {
     "msg": "Credenciais Inválidas.",
@@ -12,9 +10,11 @@ msgErr = {
 def m_user():
     try:
         data = request.get_json()
-
-        if data["name"] is None or len(data["name"]) < 4:
+        if request.path == "/createuser" and (
+            data["name"] is None or len(data["name"]) < 4
+        ):
             return msgErr
+        # Falta válidar domino de email
         if data["email"] is None or valid_email(data["email"]) is not True:
             return msgErr
         if data["password"] is None or valid_psw(data["password"]) is not True:
@@ -22,7 +22,7 @@ def m_user():
 
     except Exception as err:
         print(
-            f"A requisição enviou [%s], mas não foi encontrado o chave [%s]"
+            f"[Middleware Login] A requisição enviou %s, mas houve um problema [%s]"
             % (data, err)
         )
         return msgErr
