52a6d73c024164662227992865413aeab0bcd8d4
marlon307 <marlon-ramosb@hotmail.com>
2022-06-25 16:18:03 -0300

    Move err to controller

--- controller/contoller_user.py
+++ controller/contoller_user.py
@@ -1,17 +1,43 @@
 from flask import request
 from service.service_user import sUser
+from flask import jsonify
 
 
 class cUser:
     def c_user_register():
-        json = request.get_json()
-        result = sUser.s_register_user(json)
-        return result
+        try:
+            json = request.get_json()
+            sUser.s_register_user(json)
+
+            return (
+                jsonify(msg="Usuário cadastrado com sucesso!", status=201),
+                201,
+            )
+        except Exception as err:
+            if err.errno == 1062:
+                return jsonify(msg="Este usuário já possui cadastro.", status=403), 403
+            print(err)
+            return jsonify(msg="Falha nossa. :(", status=500), 500
 
     def c_user_login():
-        json = request.get_json()
-        result = sUser.s_login_user(json)
-        return result
+        try:
+            json = request.get_json()
+            result = sUser.s_login_user(json)
+            if result:
+                return (
+                    jsonify(
+                        user=result["info_login"],
+                        msg="Usuário logado com sucesso.",
+                        token=result["token"],
+                        status=200,
+                    ),
+                    200,
+                )
+            else:
+                return {"msg": "Dados Inválidos.", "status": 400}, 400
+        except Exception as err:
+            print(err)
+            return {"msg": "Dados Inválidos.", "status": 400}, 400
 
     def c_user_resetpsw(email, token):
         # json = request.get_json()
