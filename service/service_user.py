c569935bc9c4a138ca2c8154a25a58d93a822420
marlon307 <marlon-ramosb@hotmail.com>
2022-07-01 19:11:13 -0300

    Register with send email confirm acc

--- service/service_user.py
+++ service/service_user.py
@@ -12,9 +12,44 @@ from mail.service_email import send_mail
 
 class sUser:
     def s_register_user(json):
+        key = Fernet.generate_key()
+
         json["id_user"] = generate_id()
         json["password"] = encrypt(json["password"])
+        json["user_token"] = key
         execut_query.insert(qUser.q_register_user(), json)
+
+        info_for_crypt = {
+            "exp": str(datetime.now() + timedelta(minutes=15)),
+            "uuid": json["id_user"],
+        }
+
+        cyper = Fernet(key)
+        genertate_hash = cyper.encrypt(str(info_for_crypt).encode("utf-8"))
+        info_token = {"rtx": genertate_hash.decode("utf-8"), "email": json["email"]}
+        token = generate_token(info_token, 0, 15)
+
+        params = {
+            "name_user": json["name"],
+            "url_confirm_acc": "%sconfirm_acc/token=%s"
+            % (os.getenv("WEB_APPLICATION_URL"), token),
+        }
+
+        send_mail(
+            "[respponse.com] Confime sua conta.",
+            json["email"],
+            "confirm_acc.html",
+            params,
+        )
+        return True
+
+    def c_user_confirmacc(json):
+        result = execut_query.selectOne(
+            qUser.q_select_emailuser(), {"email": json["email"]}
+        )
+        execut_query.update(
+            qUser.q_update_active_acc(), {"email": result["email"], "key": json["key"]}
+        )
         return True
 
     def s_login_user(json):
@@ -71,8 +106,8 @@ class sUser:
         result = execut_query.selectOne(
             qUser.q_select_k_userpsw(), {"email": data["email"]}
         )
-        if result["key_resetpsw"] != data["rtx"]:
-            cyper = Fernet(str(result["key_resetpsw"]).encode("utf-8"))
+        if result["user_token"] != data["rtx"]:
+            cyper = Fernet(str(result["user_token"]).encode("utf-8"))
             decrypt = cyper.decrypt(str(data["rtx"]).encode("utf-8"))
             object_decrypt = decrypt.decode("utf-8")
             # Transforma minha string em objeto
@@ -88,7 +123,7 @@ class sUser:
                     qUser.q_update_psw_user(),
                     {
                         "password": new_psw,
-                        "key_resetpsw": data["rtx"],
+                        "user_token": data["rtx"],
                         "id_user": new_object["uuid"],
                         "email": data["email"],
                     },
