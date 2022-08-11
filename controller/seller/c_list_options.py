20000f7601f8debcb565239569ba10bbbbbc2e7d
marlon307 <marlon-ramosb@hotmail.com>
2022-08-11 19:34:05 -0300

    Add ids list and err status response

--- controller/seller/c_list_options.py
+++ controller/seller/c_list_options.py
@@ -6,9 +6,9 @@ class cOptions:
     def c_options():
         try:
             list_options = sSeller.s_list_option()
-            return {"msg": "Tamanho criado.", "list": list_options, "status": 200}, 200
+            return {"msg": "Lista de opções.", "list": list_options, "status": 200}, 200
         except Exception as err:
             if err.errno == 1062:
                 return {"msg": "Este tamnho já existe.", "status": 409}, 409
-            print("cms -> c_size ->", err)
+            print("seller -> c_list_options_seller_register_product ->", err)
             return {"msg": "Falha nossa.", "status": 500}, 500
--- models/model_seller.py
+++ models/model_seller.py
@@ -3,8 +3,8 @@ class qSeller:
         return (
             "SELECT list_colors.list_colors, list_categorys.list_ctg, list_gender.list_gender, list_sizes.list_sizes "
             "FROM "
-            "(SELECT JSON_ARRAYAGG(JSON_OBJECT('url_image',  c.url_image, 'category_name' , c.category_name, 'sub_title', c.sub_title, 'color', c.color)) AS list_ctg FROM categorys AS c) AS list_categorys, "
+            "(SELECT JSON_ARRAYAGG(JSON_OBJECT('id',  c.id, 'url_image',  c.url_image, 'category_name' , c.category_name, 'sub_title', c.sub_title, 'color', c.color)) AS list_ctg FROM categorys AS c) AS list_categorys, "
             "(SELECT JSON_ARRAYAGG(JSON_OBJECT('id',  c.id, 'color_name' , c.color_name, 'color', c.color)) AS list_colors FROM colors AS c) AS list_colors, "
-            "(SELECT JSON_ARRAYAGG(JSON_OBJECT('gender' , g.gender, 'gender_name', g.name_gender)) AS list_gender FROM gender AS g) AS list_gender, "
-            "(SELECT JSON_ARRAYAGG(JSON_OBJECT('id' , s.id, 'gender_name', s.size)) AS list_sizes FROM sizes AS s) AS list_sizes"
+            "(SELECT JSON_ARRAYAGG(JSON_OBJECT('id' , g.id, 'gender', g.gender, 'gender_name', g.name_gender)) AS list_gender FROM gender AS g) AS list_gender, "
+            "(SELECT JSON_ARRAYAGG(JSON_OBJECT('id' , s.id, 'size', s.size)) AS list_sizes FROM sizes AS s) AS list_sizes"
         )
