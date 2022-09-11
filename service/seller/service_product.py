6005511a173fe18f1acdf4363590abcc64062340
marlon307 <marlon-ramosb@hotmail.com>
2022-09-11 09:58:50 -0300

    Upload sucess one image imgur

--- service/seller/service_product.py
+++ service/seller/service_product.py
@@ -1,3 +1,4 @@
+from base64 import b64encode
 from flask import request
 import os
 from models.database import execut_query
@@ -8,6 +9,24 @@ import json
 
 class sProduct:
     def s_create_product(data, files_list):
+        # https://api.imgur.com/oauth2/authorize?client_id=4d5605fbedd6974&response_type=token
+        url = "https://api.imgur.com/3/image"
+        headers = {"Authorization": f"Bearer %" % os.getenv("AUTH_TOKEN_IMGUR")}
+        payload = {
+            "name": "Teste",
+            "title": "title teste",
+            "description": "description",
+            "image": "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+        }
+        response = requests.post(
+            url=url,
+            headers=headers,
+            data=payload,
+            # files=[files_list],
+        )
+
+        print(response.json())
+
         data = json.loads(data)
         data["id_user"] = request.headers["user"]["id_user"]
 
@@ -31,18 +50,5 @@ class sProduct:
             qProduct.q_insert_product_quantity(), format_size
         )
         print(files_list)
-        url = "https://api.imgur.com/3/upload"
-        files = []
-        payload = {"image": files}
-        headers = {"Authorization": f"Client-ID %s" % os.getenv("IMIGUR_CLIENT_ID")}
-
-        response = requests.request(
-            "POST",
-            url,
-            headers=headers,
-            data=payload,
-            files=files,
-        )
-        print(response.text)
 
         return product_id
