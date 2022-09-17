2b59ae3b5efaebe88620e2506fbed5b09dfb0555
marlon307 <marlon-ramosb@hotmail.com>
2022-09-17 14:15:46 -0300

    Inser restructure product

--- models/model_product.py
+++ models/model_product.py
@@ -6,18 +6,25 @@ class qProduct:
             "VALUES (%(categorys_id)s, %(gender_id)s, (SELECT id FROM respponse_db.user WHERE id_user = %(id_user)s), %(title)s, %(warranty)s, %(details)s, %(specifications)s)"
         )
 
-    def q_insert_product_quantity():
+    def q_insert_product_option():
         return (
-            "INSERT INTO quantity "
-            "(sizes_id, colors_id, products_id, quantity, price, discount, url_image, sku) "
-            "VALUES (%(sizes_id)s, %(colors_id)s, %(products_id)s, %(quantity)s, %(price)s, %(discount)s, %(url_image)s, %(sku)s)"
+            "INSERT INTO options_product "
+            "(colors_id, products_id, quantity, price, discount, url_image, sku) "
+            "VALUES (%(colors_id)s, %(products_id)s, %(quantity)s, %(price)s, %(discount)s, %(url_image)s, %(sku)s)"
+        )
+
+    def q_insert_option_has_sizes():
+        return (
+            "INSERT INTO options_product_has_sizes "
+            "(options_product_id, sizes_id) "
+            "VALUES (%(options_product_id)s, %(sizes_id)s)"
         )
 
     def q_insert_image():
         return (
             "INSERT INTO products_images "
-            "(quantity_id, url_image, key_img, upload_id) "
-            "VALUES (%(quantity_id)s, %(url_image)s, %(key_img)s, %(upload_id)s)"
+            "(option_id, url_image, key_img, upload_id) "
+            "VALUES (%(option_id)s, %(url_image)s, %(key_img)s, %(upload_id)s)"
         )
 
     def q_list_prod():
