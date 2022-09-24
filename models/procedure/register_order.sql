b1ad7d5525393980dd16bda553e528609a4b5810
marlon307 <marlon-ramosb@hotmail.com>
2022-09-24 09:59:19 -0300

    MSG error register prodcut in procedure type

--- models/procedure/register_order.sql
+++ models/procedure/register_order.sql
@@ -1,3 +1,4 @@
+-- https://stackoverflow.com/questions/26338033/mysql-stored-procedure-print-error-message-and-rollback
 CREATE DEFINER=`root`@`localhost` PROCEDURE `register_order`(
 p_userid CHAR(36), p_addressid INT, p_carriesid INT, p_deliveryvalue FLOAT)
 BEGIN
@@ -9,7 +10,7 @@ BEGIN
     
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
-        SHOW ERRORS;  -- this is the only one which you need
+        SHOW ERRORS;
         ROLLBACK;   
     END; 
     
@@ -20,14 +21,18 @@ BEGIN
 				 WHERE b.user_id = iduser AND b.orders_id IS NULL);
 	SET quantityvols = (SELECT SUM(quantity) FROM bag AS b WHERE b.user_id = iduser AND b.orders_id IS NULL); 
     
-    START TRANSACTION;
-		INSERT INTO orders (user_id, address_id, carrier_id, status_id, value_order, quantity_vol, delivery_value) 
-		VALUES (iduser, idaddres, p_carriesid, 1, price, quantityvols, p_deliveryvalue);
-		SET idorder = (SELECT LAST_INSERT_ID() FROM orders AS o WHERE o.user_id = iduser LIMIT 1);
-		
-		UPDATE bag AS b 
-		INNER JOIN options_product_has_sizes AS opsz ON opsz.options_product_id = b.option_product_id AND opsz.sizes_id = b.sizes_id
-		SET b.orders_id = idorder, opsz.quantity = opsz.quantity - b.quantity 
-		WHERE b.user_id = iduser AND b.orders_id IS NULL;
-	COMMIT;
+    IF price IS NOT NULL AND idaddres IS NOT NULL AND iduser IS NOT NULL THEN
+		START TRANSACTION;
+			INSERT INTO orders (user_id, address_id, carrier_id, status_id, value_order, quantity_vol, delivery_value) 
+			VALUES (iduser, idaddres, p_carriesid, 1, price, quantityvols, p_deliveryvalue);
+			SET idorder = (SELECT LAST_INSERT_ID() FROM orders AS o WHERE o.user_id = iduser LIMIT 1);
+			
+			UPDATE bag AS b 
+			INNER JOIN options_product_has_sizes AS opsz ON opsz.options_product_id = b.option_product_id AND opsz.sizes_id = b.sizes_id
+			SET b.orders_id = idorder, opsz.quantity = opsz.quantity - b.quantity 
+			WHERE b.user_id = iduser AND b.orders_id IS NULL;
+		COMMIT;
+	ELSE
+		SELECT '400 - Não possui informações o suficiente para registrar um pedido.' AS MSG;
+	END IF;
 END
