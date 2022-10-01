462336543d636a81f55d0405b00b205d9880c0be
marlon307 <marlon-ramosb@hotmail.com>
2022-09-30 21:53:31 -0300

    Start migrate fastapi

---
+++ routes/user/routes.py
@@ -0,0 +1,13 @@
+from .user import user_routes
+from .address import address_blueprint
+from .cards import cards_blueprint
+from .bag import bag_blueprint
+from .orders import orders_blueprint
+
+
+def routes_user(app):
+    user_routes(app)
+    # app.register_blueprint(address_blueprint)
+    # app.register_blueprint(cards_blueprint)
+    # app.register_blueprint(bag_blueprint)
+    # app.register_blueprint(orders_blueprint)
