9dadee0535660000a19b02ac7f80800f8bd58739
marlon307 <marlon-ramosb@hotmail.com>
2022-10-25 09:09:03 -0300

    fix select prod min detail an add query select ctg's

--- models/model_category.py
+++ models/model_category.py
@@ -2,3 +2,5 @@ q_insert_category = (
     "INSERT INTO categorys (url_image, category_name, sub_title, path, color)"
     "VALUES (%(c_image)s, %(c_name)s, %(c_title)s, %(c_path)s, %(c_color)s)"
 )
+
+q_select_categorys = "SELECT url_image, category_name, path, color FROM categorys"
