882efac523404b4c1168b083720c75f98dc5dcbf
marlon307 <marlon-ramosb@hotmail.com>
2022-12-05 12:03:38 -0300

    update requirements server

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", env_file=".env", reload=True)
+    uvicorn.run("main:app", reload=True)
+    # uvicorn.run("main:app", env_file=".env", reload=True) #Windwos
