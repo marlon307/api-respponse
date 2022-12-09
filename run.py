141c4b3b4ce01a15f6f194c78541d5c713861a26
marlon307 <marlon-ramosb@hotmail.com>
2022-12-09 09:46:28 -0300

    UYpdate run app linux

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
-    # uvicorn.run("main:app", env_file=".env", reload=True) #Windwos
+    # uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", env_file=".env", reload=True) #Windwos
