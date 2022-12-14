cc4690c3882aaf0aff58f275c5fdc73bde3617f3
marlon307 <marlon-ramosb@hotmail.com>
2022-12-14 15:28:37 -0300

    Bag query

--- models/model_bag.py
+++ models/model_bag.py
@@ -1,14 +1,15 @@
 q_insert_bag = """INSERT INTO bag (user_id, quantity, option_product_id, sizes_id) 
     VALUES ((SELECT id FROM user WHERE id_user = %(user_id)s LIMIT 1), %(quantity)s, %(product_option)s, (SELECT id FROM sizes WHERE size = %(size)s))"""
 
-q_list_bag = """SELECT p.id, o.id AS opt_id, b.quantity, s.size, o.price, o.discount, p.title, ctg.category_name, cl.color, cl.color_name, pi.url_image, b.user_id 
-    FROM bag AS b 
-    INNER JOIN sizes AS s ON s.id = b.sizes_id 
-    INNER JOIN options_product AS o ON o.id = b.option_product_id 
-    INNER JOIN colors AS cl ON cl.id = o.colors_id 
-    INNER JOIN products AS p ON p.id = o.products_id 
-    INNER JOIN categorys AS ctg ON ctg.id = p.categorys_id 
-    INNER JOIN products_images AS pi ON pi.option_id = o.id 
+q_list_bag = """SELECT p.id, op.id AS opt_id, op.products_id, p.title, b.quantity, s.size, op.price, 
+    op.discount, c.color, c.color_name, ctg.category_name, img.url_image
+    FROM respponse_db.bag AS b
+    INNER JOIN sizes AS s ON s.id = b.sizes_id
+    INNER JOIN options_product AS op ON op.id = b.option_product_id
+    INNER JOIN products AS p ON p.id = op.products_id
+    INNER JOIN colors AS c ON c.id = op.colors_id
+    INNER JOIN categorys As ctg ON ctg.id = p.categorys_id
+    INNER JOIN LATERAL (SELECT option_id, url_image FROM products_images LIMIT 1) AS img ON img.option_id = op.id
     WHERE b.user_id = (SELECT id FROM user WHERE id_user = %(user_id)s LIMIT 1) 
     AND b.orders_id IS NULL"""
 
