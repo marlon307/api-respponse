ade425e506f3a33e131c9bd1806ffce3a3be0669
marlon307 <marlon-ramosb@hotmail.com>
2022-12-18 11:30:42 -0300

    Creat produt in pg

--- routes/seller/product.py
+++ routes/seller/product.py
@@ -3,35 +3,10 @@ import json
 from pydantic import ValidationError
 from controller.seller import c_product
 from middleware.m_auth import User, get_current_adm
-from middleware.seller.m_product import m_create_product
+from ..seller.models.md_product import CreateProduct
 from .models import Default, ProductId, ListProduct, OptionProduct
 
 router = APIRouter(tags=["SELLER"])
-
-format_str = {
-    "categorys_id": 0,
-    "gender_id": 0,
-    "title": "Tilte",
-    "subTitle": "subTitle",
-    "warranty": 0,
-    "details": "Details ...",
-    "specifications": "Specifications ...",
-    "list_qtd": [
-        {
-            "id": 0,
-            "color": "#EXA",
-            "sizes_id": [
-                {
-                    "id": 0,
-                    "quantity": 0,
-                }
-            ],
-            "price": 0,
-            "discount": 0,
-            "sku": "SKU",
-        }
-    ],
-}
 
 msgErr415 = HTTPException(
     status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
@@ -39,21 +14,16 @@ msgErr415 = HTTPException(
 )
 
 
-@router.post("/product", response_model=Default, status_code=201)
+@router.post("/product", status_code=201)
 def create_product(
-    file: list[bytes] = File(description="Multiple files as UploadFile"),
-    data: str = Form(
-        default=json.dumps(format_str),
-        description="Copie as informaçoes do input e altere os valores mantendo o formato (STRING/JSON)",
-    ),
+    files: list[bytes] = File(),
+    data: CreateProduct = Depends(CreateProduct.fields_product),
     current_user: User = Depends(get_current_adm),
 ):
     try:
-        # validar informações do
-        serialize_json = json.loads(data)
+        serialize_json = json.loads(data.json())
         serialize_json["id_user"] = current_user.id_user
-        n_data = m_create_product(**serialize_json)
-        return c_product.product(n_data.dict(), file)
+        return c_product.product(serialize_json, files)
     except ValidationError as e:
         print("Route -> product: ", e.raw_errors)
         raise msgErr415
@@ -64,11 +34,11 @@ def list_product():
     return c_product.list_product()
 
 
+@router.get("/product/list_options")
+def list_options():
+    return c_product.options()
+
+
 @router.get("/product/{id}", response_model=ProductId)
 def get_product_id(id: int):
     return c_product.get_product_id(id)
-
-
-@router.get("/product/list_options")
-def list_options(current_user: User = Depends(get_current_adm)):
-    return c_product.options()
