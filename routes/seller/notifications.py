bed8db7ee272f730182de83a34a88b70233862f9
marlon307 <marlon-ramosb@hotmail.com>
2023-01-25 12:50:35 -0300

    send notify types query

--- routes/seller/notifications.py
+++ routes/seller/notifications.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from controller.seller import c_notification
 from middleware.m_auth import User, get_current_adm
 from ..user.models.md_notification import DataNotification
@@ -10,13 +10,17 @@ router = APIRouter(tags=["SELLER"])
 # /notification?data.id=1312077069&order_id=200&type=payment
 
 
+# def teste(id):
+#     return id
+
+
 @router.post("/notification")
 def seller_notification_paymnet(
     order_id: int,
-    data: DataNotification,
-    topic: str | None,
-    id: int | None,
-    type: str | None,
+    type: str = Query(None),
+    id: int = Query(None),
+    topic: str = Query(None),
+    data: int = Query(None, alias="data.id"),
 ):
-    print(order_id, data, type)
+    print(order_id, type, data, 111)
     return c_notification.seller_notification(order_id, topic, id)
--- routes/user/models/md_notification.py
+++ routes/user/models/md_notification.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, create_model
 
 
 class DataNotification(BaseModel):
